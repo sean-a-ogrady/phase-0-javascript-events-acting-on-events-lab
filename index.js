67ed303e5e92ced600a10d4bab3d2ee61cdf6efd
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-08-28 08:33:17 -0400

    Implement move functions

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers);
+    if (left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+//const game = document.getElementById('game');
+//const computedGameWidth = getComputedStyle(game).width;
+//const gameWidth = parseInt(computedGameWidth.replace('px', ''));
+//const computedDodgerWidth = getComputedStyle(dodger).width;
+//const dodgerWidth = parseInt(computedDodgerWidth.replace('px', ''));
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers);
+    if (left < 360/*gameWidth - dodgerWidth*/){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowLeft'){
+        moveDodgerLeft();
+    }
+    if (e.key === 'ArrowRight'){
+        moveDodgerRight();
+    }
+});
